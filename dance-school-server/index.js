96ace82692a1a351233565c73445658ce4b1c96b
senior9 <ovi215188@gmail.com>
2023-06-14 22:58:05 +0600

    serverside continue

--- dance-school-server/index.js
+++ dance-school-server/index.js
@@ -47,7 +47,7 @@ async function run() {
 // JWT 
 app.post('/jwt',(req,res)=>{
   const user = req.body ;
-  const token = jwt.sign(user,env.process.ACCESS_TOKEN,{expiresIn:'1h'})
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
   res.send(token)
 })
 
